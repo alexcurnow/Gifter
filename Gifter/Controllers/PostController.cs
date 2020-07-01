b17090e677843617cc188e8aef720513773f728d
Alex Curnow <alexcurnow@gmail.com>
2020-07-01 15:53:30 -0500

    users can now add new posts and search existing posts

--- Gifter/Controllers/PostController.cs
+++ Gifter/Controllers/PostController.cs
@@ -65,6 +65,20 @@ namespace Gifter.Controllers
             return NoContent();
         }
 
+        [HttpGet("search")]
+        public IActionResult Search(string q, bool sortDesc)
+        {
+            if (q == null)
+            {
+                return Ok(_postRepository.GetAll());
+
+            }
+            else
+            {
+            return Ok(_postRepository.Search(q, sortDesc));
+            }
+        }
+
         [HttpGet("hottest")]
         public IActionResult Hottest(DateTime q)
         {
